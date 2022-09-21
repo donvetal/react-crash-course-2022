109e11ad639cd01a97d9339681a497bc2d164adb
Vitaliy <vitaliymontana@yandex.ru>
2022-09-21 19:39:11 +0300

    First commit

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque doloribus excepturi illo iste voluptate.
+            Ad debitis dolor, error id magnam pariatur placeat. Aliquam, architecto aspernatur consequuntur dolor
+            doloremque earum iure magnam non quaerat repudiandae, similique, sint! Aperiam consequuntur doloribus error
+            expedita fuga fugiat nihil non praesentium quo rerum? Minus, quaerat.</div>
+    )
+}
